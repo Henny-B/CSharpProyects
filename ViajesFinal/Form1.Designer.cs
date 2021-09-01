ef4002a0b46e3e004f7cf6aa5d7b2b39ecfdaf45
Henny Bustamante <henny.bustamante@gmail.com>
2021-09-01 00:09:55 -0300

    pusheando proyectos

---
+++ ViajesFinal/Form1.Designer.cs
@@ -0,0 +1,263 @@
+﻿
+namespace ViajesFinal
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSalir = new System.Windows.Forms.Button();
+            this.btnGrabar = new System.Windows.Forms.Button();
+            this.btnNuevo = new System.Windows.Forms.Button();
+            this.lstViajes = new System.Windows.Forms.ListBox();
+            this.dtpFecha = new System.Windows.Forms.DateTimePicker();
+            this.rbtInternacional = new System.Windows.Forms.RadioButton();
+            this.rbtNacional = new System.Windows.Forms.RadioButton();
+            this.cboTransporte = new System.Windows.Forms.ComboBox();
+            this.txtDestino = new System.Windows.Forms.TextBox();
+            this.txtCodigo = new System.Windows.Forms.TextBox();
+            this.lblFecha = new System.Windows.Forms.Label();
+            this.lblTipo = new System.Windows.Forms.Label();
+            this.lblTransporte = new System.Windows.Forms.Label();
+            this.lblDestino = new System.Windows.Forms.Label();
+            this.lblCodigo = new System.Windows.Forms.Label();
+            this.btnEditar = new System.Windows.Forms.Button();
+            this.btnCancelar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.Location = new System.Drawing.Point(429, 293);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(75, 23);
+            this.btnSalir.TabIndex = 11;
+            this.btnSalir.Text = "Salir";
+            this.btnSalir.UseVisualStyleBackColor = true;
+            this.btnSalir.Click += new System.EventHandler(this.btnSalir_Click);
+            // 
+            // btnGrabar
+            // 
+            this.btnGrabar.Enabled = false;
+            this.btnGrabar.Location = new System.Drawing.Point(204, 293);
+            this.btnGrabar.Name = "btnGrabar";
+            this.btnGrabar.Size = new System.Drawing.Size(75, 23);
+            this.btnGrabar.TabIndex = 9;
+            this.btnGrabar.Text = "Grabar";
+            this.btnGrabar.UseVisualStyleBackColor = true;
+            this.btnGrabar.Click += new System.EventHandler(this.btnGrabar_Click);
+            // 
+            // btnNuevo
+            // 
+            this.btnNuevo.Location = new System.Drawing.Point(30, 293);
+            this.btnNuevo.Name = "btnNuevo";
+            this.btnNuevo.Size = new System.Drawing.Size(75, 23);
+            this.btnNuevo.TabIndex = 7;
+            this.btnNuevo.Text = "Nuevo";
+            this.btnNuevo.UseVisualStyleBackColor = true;
+            this.btnNuevo.Click += new System.EventHandler(this.btnNuevo_Click);
+            // 
+            // lstViajes
+            // 
+            this.lstViajes.FormattingEnabled = true;
+            this.lstViajes.Location = new System.Drawing.Point(338, 31);
+            this.lstViajes.Name = "lstViajes";
+            this.lstViajes.Size = new System.Drawing.Size(166, 199);
+            this.lstViajes.TabIndex = 25;
+            this.lstViajes.SelectedIndexChanged += new System.EventHandler(this.lstViajes_SelectedIndexChanged);
+            // 
+            // dtpFecha
+            // 
+            this.dtpFecha.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dtpFecha.Location = new System.Drawing.Point(106, 221);
+            this.dtpFecha.Name = "dtpFecha";
+            this.dtpFecha.Size = new System.Drawing.Size(97, 20);
+            this.dtpFecha.TabIndex = 6;
+            // 
+            // rbtInternacional
+            // 
+            this.rbtInternacional.AutoSize = true;
+            this.rbtInternacional.Location = new System.Drawing.Point(204, 180);
+            this.rbtInternacional.Name = "rbtInternacional";
+            this.rbtInternacional.Size = new System.Drawing.Size(101, 17);
+            this.rbtInternacional.TabIndex = 5;
+            this.rbtInternacional.TabStop = true;
+            this.rbtInternacional.Text = "2 - Internacional";
+            this.rbtInternacional.UseVisualStyleBackColor = true;
+            // 
+            // rbtNacional
+            // 
+            this.rbtNacional.AutoSize = true;
+            this.rbtNacional.Location = new System.Drawing.Point(106, 180);
+            this.rbtNacional.Name = "rbtNacional";
+            this.rbtNacional.Size = new System.Drawing.Size(82, 17);
+            this.rbtNacional.TabIndex = 4;
+            this.rbtNacional.TabStop = true;
+            this.rbtNacional.Text = "1 - Nacional";
+            this.rbtNacional.UseVisualStyleBackColor = true;
+            // 
+            // cboTransporte
+            // 
+            this.cboTransporte.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cboTransporte.FormattingEnabled = true;
+            this.cboTransporte.Location = new System.Drawing.Point(106, 136);
+            this.cboTransporte.Name = "cboTransporte";
+            this.cboTransporte.Size = new System.Drawing.Size(163, 21);
+            this.cboTransporte.TabIndex = 3;
+            // 
+            // txtDestino
+            // 
+            this.txtDestino.Location = new System.Drawing.Point(106, 82);
+            this.txtDestino.Name = "txtDestino";
+            this.txtDestino.Size = new System.Drawing.Size(163, 20);
+            this.txtDestino.TabIndex = 2;
+            // 
+            // txtCodigo
+            // 
+            this.txtCodigo.Location = new System.Drawing.Point(106, 31);
+            this.txtCodigo.Name = "txtCodigo";
+            this.txtCodigo.Size = new System.Drawing.Size(57, 20);
+            this.txtCodigo.TabIndex = 1;
+            // 
+            // lblFecha
+            // 
+            this.lblFecha.AutoSize = true;
+            this.lblFecha.Location = new System.Drawing.Point(27, 228);
+            this.lblFecha.Name = "lblFecha";
+            this.lblFecha.Size = new System.Drawing.Size(37, 13);
+            this.lblFecha.TabIndex = 19;
+            this.lblFecha.Text = "Fecha";
+            // 
+            // lblTipo
+            // 
+            this.lblTipo.AutoSize = true;
+            this.lblTipo.Location = new System.Drawing.Point(27, 182);
+            this.lblTipo.Name = "lblTipo";
+            this.lblTipo.Size = new System.Drawing.Size(28, 13);
+            this.lblTipo.TabIndex = 17;
+            this.lblTipo.Text = "Tipo";
+            // 
+            // lblTransporte
+            // 
+            this.lblTransporte.AutoSize = true;
+            this.lblTransporte.Location = new System.Drawing.Point(27, 139);
+            this.lblTransporte.Name = "lblTransporte";
+            this.lblTransporte.Size = new System.Drawing.Size(58, 13);
+            this.lblTransporte.TabIndex = 16;
+            this.lblTransporte.Text = "Transporte";
+            // 
+            // lblDestino
+            // 
+            this.lblDestino.AutoSize = true;
+            this.lblDestino.Location = new System.Drawing.Point(27, 89);
+            this.lblDestino.Name = "lblDestino";
+            this.lblDestino.Size = new System.Drawing.Size(43, 13);
+            this.lblDestino.TabIndex = 14;
+            this.lblDestino.Text = "Destino";
+            // 
+            // lblCodigo
+            // 
+            this.lblCodigo.AutoSize = true;
+            this.lblCodigo.Location = new System.Drawing.Point(27, 34);
+            this.lblCodigo.Name = "lblCodigo";
+            this.lblCodigo.Size = new System.Drawing.Size(40, 13);
+            this.lblCodigo.TabIndex = 12;
+            this.lblCodigo.Text = "Código";
+            // 
+            // btnEditar
+            // 
+            this.btnEditar.Enabled = false;
+            this.btnEditar.Location = new System.Drawing.Point(113, 293);
+            this.btnEditar.Name = "btnEditar";
+            this.btnEditar.Size = new System.Drawing.Size(75, 23);
+            this.btnEditar.TabIndex = 8;
+            this.btnEditar.Text = "Editar";
+            this.btnEditar.UseVisualStyleBackColor = true;
+            this.btnEditar.Click += new System.EventHandler(this.btnEditar_Click);
+            // 
+            // btnCancelar
+            // 
+            this.btnCancelar.Enabled = false;
+            this.btnCancelar.Location = new System.Drawing.Point(348, 293);
+            this.btnCancelar.Name = "btnCancelar";
+            this.btnCancelar.Size = new System.Drawing.Size(75, 23);
+            this.btnCancelar.TabIndex = 10;
+            this.btnCancelar.Text = "Cancelar";
+            this.btnCancelar.UseVisualStyleBackColor = true;
+            this.btnCancelar.Click += new System.EventHandler(this.btnCancelar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(541, 341);
+            this.Controls.Add(this.btnCancelar);
+            this.Controls.Add(this.btnEditar);
+            this.Controls.Add(this.btnSalir);
+            this.Controls.Add(this.btnGrabar);
+            this.Controls.Add(this.btnNuevo);
+            this.Controls.Add(this.lstViajes);
+            this.Controls.Add(this.dtpFecha);
+            this.Controls.Add(this.rbtInternacional);
+            this.Controls.Add(this.rbtNacional);
+            this.Controls.Add(this.cboTransporte);
+            this.Controls.Add(this.txtDestino);
+            this.Controls.Add(this.txtCodigo);
+            this.Controls.Add(this.lblFecha);
+            this.Controls.Add(this.lblTipo);
+            this.Controls.Add(this.lblTransporte);
+            this.Controls.Add(this.lblDestino);
+            this.Controls.Add(this.lblCodigo);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSalir;
+        private System.Windows.Forms.Button btnGrabar;
+        private System.Windows.Forms.Button btnNuevo;
+        private System.Windows.Forms.ListBox lstViajes;
+        private System.Windows.Forms.DateTimePicker dtpFecha;
+        private System.Windows.Forms.RadioButton rbtInternacional;
+        private System.Windows.Forms.RadioButton rbtNacional;
+        private System.Windows.Forms.ComboBox cboTransporte;
+        private System.Windows.Forms.TextBox txtDestino;
+        private System.Windows.Forms.TextBox txtCodigo;
+        private System.Windows.Forms.Label lblFecha;
+        private System.Windows.Forms.Label lblTipo;
+        private System.Windows.Forms.Label lblTransporte;
+        private System.Windows.Forms.Label lblDestino;
+        private System.Windows.Forms.Label lblCodigo;
+        private System.Windows.Forms.Button btnEditar;
+        private System.Windows.Forms.Button btnCancelar;
+    }
+}
+
---
+++ ViajesFinal/Form1.cs
@@ -0,0 +1,304 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace ViajesFinal
+{
+    public partial class Form1 : Form
+    {
+        Viaje[] aViaje = new Viaje[300];
+        int contador;
+        bool nuevo = false;
+
+
+        SqlConnection cnn = new SqlConnection(@"Data Source=.\SQLEXPRESS;Initial Catalog=AGENCIAVIAJES;Integrated Security=True");
+        SqlCommand cmd = new SqlCommand();
+        SqlDataReader reader;
+
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            habilitar(false);
+            CargarCombo(cboTransporte, "Transportes", "nombreTransporte", "idTransporte");
+            cboTransporte.SelectedIndex = -1;
+            CargarLista(lstViajes, "Viajes");
+            btnNuevo.Focus();
+
+
+        }
+
+        private void habilitar(bool x)
+        {
+
+            txtCodigo.Enabled = x;
+            txtDestino.Enabled = x;
+            cboTransporte.Enabled = x;            
+            rbtInternacional.Enabled = x;
+            rbtNacional.Enabled = x;
+            dtpFecha.Enabled = x;
+            btnNuevo.Enabled = !x;
+            btnGrabar.Enabled = !x;
+            btnEditar.Enabled = !x;
+            btnCancelar.Enabled = !x;            
+            btnSalir.Enabled = !x;
+
+
+        }
+
+
+        private void limpiar()
+        {
+            txtCodigo.Text = "";
+            txtDestino.Text = "";
+            cboTransporte.SelectedIndex = -1;
+            rbtInternacional.Checked = false;
+            rbtNacional.Checked = false;
+            txtCodigo.Focus();
+
+        }
+
+
+        private void CargarCombo(ComboBox combo, string nombTabla, string displayMember, string valueMember)
+        {
+            DataTable tabla = ConsultarTabla(nombTabla);
+            combo.DataSource = tabla;
+            combo.DisplayMember = displayMember;
+            combo.ValueMember = valueMember;
+            combo.DropDownStyle = ComboBoxStyle.DropDownList;
+
+        }
+
+        private DataTable ConsultarTabla(string nombreTabla)
+        {
+            cnn.Open();
+
+            cmd.Connection = cnn;
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "SELECT * FROM " + nombreTabla;
+
+            DataTable tabla = new DataTable();
+            tabla.Load(cmd.ExecuteReader());
+
+            cnn.Close();
+
+            return tabla;
+
+        }
+
+
+        private void CargarLista(ListBox lista, string nombTabla)
+        {
+            contador = 0;
+
+            cnn.Open();
+            cmd.Connection = cnn;
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "SELECT * FROM " + nombTabla;
+            reader = cmd.ExecuteReader();
+
+            while (reader.Read())
+            {
+
+                Viaje v = new Viaje();
+                if (!reader.IsDBNull(0))
+                    v.pCodigo = reader.GetInt32(0);
+                if (!reader.IsDBNull(1))
+                    v.pDestino = reader.GetString(1);
+                if (!reader.IsDBNull(2))
+                    v.pTransporte = reader.GetInt32(2);
+                if (!reader.IsDBNull(3))
+                    v.pTipo = reader.GetInt32(3);                    
+                if (!reader.IsDBNull(4))
+                    v.pFecha = reader.GetDateTime(4);
+
+                aViaje[contador] = v;
+                contador++;
+
+            }
+
+            reader.Close();
+            cnn.Close();
+            lista.Items.Clear();
+
+            for (int i = 0; i < contador; i++)
+            {
+                lista.Items.Add(aViaje[i].ToString());
+
+            }
+            lista.SelectedIndex = 0;
+        }
+
+        private void btnNuevo_Click(object sender, EventArgs e)
+        {
+            nuevo = true;
+            habilitar(true);
+            limpiar();
+            btnGrabar.Enabled = true;
+            btnCancelar.Enabled = true;
+            btnSalir.Enabled = true;
+            txtCodigo.Focus();
+        }
+
+        private void btnEditar_Click(object sender, EventArgs e)
+        {
+            habilitar(true);
+            txtCodigo.Enabled = false;
+            txtDestino.Focus();
+            btnCancelar.Enabled = true;
+            btnGrabar.Enabled = true;
+        }
+
+        private void btnGrabar_Click(object sender, EventArgs e)
+        {
+
+
+            Viaje v = new Viaje();
+            v.pCodigo = Convert.ToInt32(txtCodigo.Text);
+            v.pDestino = txtDestino.Text;
+            v.pTransporte = Convert.ToInt32(cboTransporte.SelectedValue);
+            if (rbtNacional.Checked)
+                v.pTipo = 1;
+            else
+                v.pTipo = 2;
+            v.pFecha = dtpFecha.Value;
+
+            if (nuevo)
+            {
+                try
+                {
+                    if (txtCodigo.Text == "" || txtDestino.Text == "")
+                    {
+                        MessageBox.Show("Olvidó ingresar un campo!");
+                        return;
+                    }
+                    if (cboTransporte.SelectedIndex < 0)
+                    {
+                        MessageBox.Show("Olvidó seleccionar una marca!");
+                        return;
+                    }
+
+                    try
+                    {
+                        int codigo = Convert.ToInt32(txtCodigo.Text);
+
+
+                    }
+                    catch (Exception)
+                    {
+                        MessageBox.Show("El código debe ser numérico!");
+                        return;
+                    }
+
+                    cnn.Open();
+                    cmd = new SqlCommand("INSERT INTO Viajes VALUES (@codigo, @destino, @transporte, @tipo, @fecha)", cnn);
+                    cmd.Parameters.AddWithValue("@codigo", txtCodigo.Text);
+                    cmd.Parameters.AddWithValue("@destino", txtDestino.Text);
+                    cmd.Parameters.AddWithValue("@transporte", cboTransporte.SelectedValue);
+                    cmd.Parameters.AddWithValue("@tipo", v.pTipo);
+                    cmd.Parameters.AddWithValue("@fecha", dtpFecha.Value);
+                    cmd.ExecuteNonQuery();
+                    cnn.Close();
+
+
+                    
+                    CargarLista(lstViajes, "Viajes");
+                    habilitar(false);
+
+                    MessageBox.Show("Viaje guardado!");
+                    limpiar();
+
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("No se pudo guardar el viaje");
+                    MessageBox.Show(ex.Message);
+                }
+
+                btnNuevo.Focus();
+            } else
+            {
+
+                cnn.Open();
+                cmd.Connection = cnn;
+                cmd.CommandType = CommandType.Text;
+                cmd.CommandText = "UPDATE Viajes SET codigo=@codigo," +
+                                                       " destino=@destino," +
+                                                       " transporte=@transporte," +
+                                                       " tipo=@tipo," +
+                                                       " fecha=@fecha" +                                                       
+                                                       " WHERE codigo=@codigo";
+                cmd.Parameters.AddWithValue("@codigo", v.pCodigo);
+                cmd.Parameters.AddWithValue("@destino",v.pDestino);
+                cmd.Parameters.AddWithValue("@transporte",v.pTransporte);
+                cmd.Parameters.AddWithValue("@tipo", v.pTipo);
+                cmd.Parameters.AddWithValue("@fecha", v.pFecha);
+
+
+                cmd.ExecuteNonQuery();
+                cnn.Close();
+
+
+
+            }
+
+                CargarLista(lstViajes, "Viajes");
+                habilitar(false);
+                nuevo = false;
+
+        }
+
+        private void cargarCampos(int posicion)
+        {
+            txtCodigo.Text = aViaje[posicion].pCodigo.ToString();
+            txtDestino.Text = aViaje[posicion].pDestino;
+            cboTransporte.SelectedValue = aViaje[posicion].pTransporte;
+            if (aViaje[posicion].pTipo == 1)
+                rbtNacional.Checked = true;
+            else
+                rbtInternacional.Checked = true;
+            dtpFecha.Value = aViaje[posicion].pFecha;
+
+
+        }
+
+
+        private void lstViajes_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            this.cargarCampos(lstViajes.SelectedIndex);
+        }
+
+
+
+        private void btnCancelar_Click(object sender, EventArgs e)
+        {
+            limpiar();
+            habilitar(false);
+            nuevo = false;
+            btnNuevo.Focus();
+        }
+
+        private void btnSalir_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Seguro de abandonar la aplicación ?",
+                    "SALIR", MessageBoxButtons.YesNo, MessageBoxIcon.Question,
+                    MessageBoxDefaultButton.Button2) == DialogResult.Yes)
+                this.Close();
+        }
+
+      
+    }
+}
